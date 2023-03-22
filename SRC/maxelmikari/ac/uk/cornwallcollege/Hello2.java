a2fdbf38773fd336a654c55177b04c14e94c374a
Maxel-Mikari <Courtneylh11.ch@gmail.com>
2023-03-22 09:39:10 +0000

    Hello2

--- SRC/maxelmikari/ac/uk/cornwallcollege/Hello2.java
+++ SRC/maxelmikari/ac/uk/cornwallcollege/Hello2.java
@@ -1,5 +1,6 @@
 package maxelmikari.ac.uk.cornwallcollege;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Hello2 {
@@ -9,10 +10,18 @@ public class Hello2 {
         int denominator;
 
         do {
-            numerator = scanner.nextInt();
-            denominator = scanner.nextInt();
-            int result = numerator / denominator;
-            System.out.println("" + numerator + " / " + denominator + " = " + result);
+            try{
+                numerator = scanner.nextInt();
+                denominator = scanner.nextInt();
+                int result = numerator / denominator;
+                System.out.println("" + numerator + " / " + denominator + " = " + result);
+            } catch (ArithmeticException ex1) {
+                System.out.println ("You tried to divide by zero");
+            } catch (InputMismatchException ex2) {
+                System.out.println("Bad input");
+                scanner.nextLine();
+            }
+
         } while (denominator != -1);
     }
 }
