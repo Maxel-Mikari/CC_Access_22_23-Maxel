5a75aaf5a3a946cffbf808322ab8e070ddc740b7
Maxel-Mikari <Courtneylh11.ch@gmail.com>
2023-03-29 11:41:05 +0100

    GameCharacter

--- SRC/maxelmikari/ac/uk/cornwallcollege/game/Game.java
+++ SRC/maxelmikari/ac/uk/cornwallcollege/game/Game.java
@@ -2,7 +2,7 @@ package maxelmikari.ac.uk.cornwallcollege.game;
 
 public class Game {
 
-    public static void main(String[] args) {
+    public static <Scanner> void main(String[] args) {
 
         GameCharacter c1 = new GameCharacter(10, 5, 4, 1, 2, "Zorro");
         System.out.println(c1.toString());
@@ -13,5 +13,23 @@ public class Game {
         GameCharacter c4 = new TeamRocket(1, 2,3,4,5,"Rockets");
         System.out.println(c4);
 
+        Scanner scan new = new Scanner(System.in);
+        System.out.println("Please select first character\n1 for a playable character\n2 for an NPC\n3 for Team Rocket");
+        int choice = scan.nextInt();
+        Character c1;
+        switch choice {
+            case 1:
+                c1 = new PlayableCharacter(5, 6, 7, 8, 9, "Safdari");
+                break;
+            case 2:
+                c1 = new NPC(1, 2, 3, 4, 5, "Joe Hill");
+                break;
+            case 3:
+                c1 = new TeamRocket(1, 2, 3 4, 5, "Rockets");
+                break;
+            default:
+                c1 = new NPC(1, 2, 3, 4, 5, "Joe Hill");
+        }
+        System.out.println("The first character is "+c1)
     }
 }
