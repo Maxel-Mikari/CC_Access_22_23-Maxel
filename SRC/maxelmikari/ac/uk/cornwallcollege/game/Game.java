c61a1cde833ea688bf92a09e05d2607aea78c2cf
Maxel-Mikari <Courtneylh11.ch@gmail.com>
2023-03-29 11:46:08 +0100

    Game2

--- SRC/maxelmikari/ac/uk/cornwallcollege/game/Game.java
+++ SRC/maxelmikari/ac/uk/cornwallcollege/game/Game.java
@@ -1,10 +1,12 @@
 package maxelmikari.ac.uk.cornwallcollege.game;
 
+import java.util.Scanner;
+
 public class Game {
 
-    public static <Scanner> void main(String[] args) {
+    public static void main(String[] args) {
 
-        GameCharacter c1 = new GameCharacter(10, 5, 4, 1, 2, "Zorro");
+        /* GameCharacter c1 = new GameCharacter(10, 5, 4, 1, 2, "Zorro");
         System.out.println(c1.toString());
         GameCharacter c2 = new PlayableCharacter(5, 6, 7, 8, 9, "Safdari");
         System.out.println(c2);
@@ -12,12 +14,13 @@ public class Game {
         System.out.println(c3);
         GameCharacter c4 = new TeamRocket(1, 2,3,4,5,"Rockets");
         System.out.println(c4);
+        */
 
-        Scanner scan new = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.println("Please select first character\n1 for a playable character\n2 for an NPC\n3 for Team Rocket");
         int choice = scan.nextInt();
-        Character c1;
-        switch choice {
+        GameCharacter c1;
+        switch (choice) {
             case 1:
                 c1 = new PlayableCharacter(5, 6, 7, 8, 9, "Safdari");
                 break;
@@ -25,11 +28,11 @@ public class Game {
                 c1 = new NPC(1, 2, 3, 4, 5, "Joe Hill");
                 break;
             case 3:
-                c1 = new TeamRocket(1, 2, 3 4, 5, "Rockets");
+                c1 = new TeamRocket(1, 2, 3, 4, 5, "Rockets");
                 break;
             default:
                 c1 = new NPC(1, 2, 3, 4, 5, "Joe Hill");
         }
-        System.out.println("The first character is "+c1)
+        System.out.println("The first character is "+c1);
     }
 }
