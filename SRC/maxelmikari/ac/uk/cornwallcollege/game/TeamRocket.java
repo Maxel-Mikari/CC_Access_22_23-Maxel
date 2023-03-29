9f7e1c25428d7775cfec302d6a7fd0cb97d0cea8
Maxel-Mikari <Courtneylh11.ch@gmail.com>
2023-03-29 12:02:47 +0100

    TeamRocket

--- SRC/maxelmikari/ac/uk/cornwallcollege/game/TeamRocket.java
+++ SRC/maxelmikari/ac/uk/cornwallcollege/game/TeamRocket.java
@@ -7,6 +7,11 @@ public class TeamRocket extends NPC{
     }
 
     @Override
+    public String toString() {
+        return "Prepare for trouble and make it double! To protect the world from devastation, to unite all peoples within our nation. To denounce the evils of truth and love, to extend our reach to the stars above. Jessie! James! Team Rocket blasts off at the speed of light. Surrender now or prepare to fight! Meowth! That's right!";
+    }
+
+    @Override
     public void dealDamage(GameCharacter c, int amount) {
         c.health -= attack;
 
